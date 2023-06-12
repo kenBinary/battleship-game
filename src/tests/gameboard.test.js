a61e0dc867a1b964c76c71453644f9440e461435
thennek0 <kennethbecaro0@gmail.com>
2023-06-12 10:55:40 +0800

    add unit test for gameboard

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -12,13 +12,23 @@ test("ship placed in specified coordinate along y axis", () => {
     expect(newGameBoard.placeShip(1, 1, newShipB, "vertical")).toEqual(["bc", "bc", "bc"])
 
 });
-// test("check if attack hit a ship based on given coordinates", () => {
-//     expect(newGameBoard.receiveAttack(1, 1)).toBe(true);
-// });
+test("check if attack hit a ship based in horizontal axis", () => {
+    expect(newGameBoard.receiveAttack(0, 0)).toBe(true);
+    expect(newGameBoard.receiveAttack(0, 1)).toBe(true);
+    expect(newGameBoard.receiveAttack(0, 2)).toBe(true);
+});
 
-// test("check if attack did not hit ship based on given coordinates", () => {
-//     expect(newGameBoard.receiveAttack(1, 1)).toBe(false)
-// });
+test("check if attack hit a ship based in vertical axis", () => {
+    expect(newGameBoard.receiveAttack(1, 1)).toBe(true);
+    expect(newGameBoard.receiveAttack(2, 1)).toBe(true);
+    expect(newGameBoard.receiveAttack(3, 1)).toBe(true);
+});
+
+test("check if attack did not hit ship based on given coordinates", () => {
+    expect(newGameBoard.receiveAttack(1, 2)).toBe(false)
+    expect(newGameBoard.receiveAttack(2, 2)).toBe(false)
+    expect(newGameBoard.receiveAttack(3, 2)).toBe(false)
+});
 // test("check if all attacked coordinates are tracked", () => {
 //     expect(true).toBe(true)
 // });
