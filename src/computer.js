4ebbd35a37e362b7c21f949a94aa1e3822ed8634
thennek0 <kennethbecaro0@gmail.com>
2023-06-17 12:17:28 +0800

    fix random unmissed shot function generator

--- src/computer.js
+++ src/computer.js
@@ -2,15 +2,12 @@ import player from "./player";
 
 const computer = () => {
     const { getTurn, getName, setTurn } = player("Computer", false);
-    // function isNestedArrayInArray(arr, target) {
-    //     return arr.some(subArray => subArray.length === target.length && subArray.every((value, index) => value === target[index]));
-    // }
-    function coordinateInMissed(arr, target) {
+    function coordinateInMissed(arr, pTarget) {
+        let target = `${pTarget[0]}${pTarget[1]}`;
         if (arr.includes(target)) {
             console.log(target)
             return true;
         }
-        // return arr.some(subArray => subArray.length === target.length && subArray.every((value, index) => value === target[index]));
     }
     function randomCoordinate() {
         let xCoordinate = Math.floor(Math.random() * 10);
@@ -23,17 +20,12 @@ const computer = () => {
         let isHit;
         while (true) {
             coordinate = randomCoordinate();
-            // if (isNestedArrayInArray(gameBoard.getMissedAttacks(), coordinate)) {
-            //     coordinate = randomCoordinate();
-            // }
             if (coordinateInMissed(gameBoard.getMissedAttacks(), coordinate)) {
-                coordinate = randomCoordinate;
+                coordinate = randomCoordinate();
             }
             else {
                 isHit = gameBoard.receiveAttack(coordinate[0], coordinate[1]);
-                // gameBoard.receiveAttack(coordinate[0], coordinate[1], ship)
-                // return gameBoard.receiveAttack(coordinate[0], coordinate[1]);
-                return [isHit,coordinate];
+                return [isHit, coordinate];
             }
         }
     }
